6ff51804b2f09f4ffe7d8b3b83393a92e8c80bd9
Harsh Bhatt <harshbhatt0151@gmail.com>
2022-01-11 20:17:02 +0530

    Create Profile.sol
    This SC creates a user and gets the user according to index numbers

---
+++ Profile.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.7.5;
+
+contract profile{
+
+    struct Men{
+        string name;
+        address id;
+    }
+
+    Men[] men;
+
+
+    mapping(address => string) person;
+
+    function setPerson(string memory _name, address _id) public {
+        Men memory _men = Men(_name, _id);
+        men.push(_men);
+    }
+
+    function getPerson(uint _index) public view returns(string memory, address){
+        return(men[_index].name, men[_index].id);
+    }
+
+    
+}
